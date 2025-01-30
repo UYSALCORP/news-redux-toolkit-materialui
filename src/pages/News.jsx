9ecd87878e6cb945d695d219a38391d11002774b
UYSALCORP <uysalcorp@gmail.com>
2025-01-30 22:56:40 +0300

    end

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,24 +1,24 @@
-
+import { useDispatch, useSelector } from "react-redux";
 import loadingGif from "../assets/loading.gif";
 
+import { Box, Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
 
-import {
-  Box,
-
-} from "@mui/material";
+import { useEffect } from "react";
+import { getirData } from "../features/haberSlice";
 
 const News = () => {
- 
- 
- 
+  const dispatch = useDispatch();
+  const { loading, haberler } = useSelector((state) => state.haberSlice);
+
+  useEffect(() => {
+    dispatch(getirData());
+  }, []);
 
   return (
     <>
-   
-       
-          <img src={""} alt="" height="800px" />
-       
-   
+      {loading ? (
+        <img src={loadingGif} alt="" height="800px" />
+      ) : (
         <Box
           xs={{ d: "flex" }}
           display="flex"
@@ -26,9 +26,31 @@ const News = () => {
           justifyContent="space-evenly"
           flexWrap="wrap"
         >
-       
+          {
+            haberler.map((haber)=>(
+              <Card sx={{ maxWidth: 345, maxHeight: 600, mt: 4}}>
+              <CardMedia
+                sx={{ height: 140 }}
+                image={haber.urlToImage}
+                title="haber"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {haber.title}
+                </Typography>
+                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                  {haber.content}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Clear</Button>
+                <Button href={haber.url} target="blank" size="small">Learn More</Button>
+              </CardActions>
+            </Card>
+            ))
+          }
         </Box>
-    News
+      )}
     </>
   );
 };
